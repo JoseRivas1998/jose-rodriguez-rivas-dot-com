4965800b9b52c1d467d339e622f6f9af4660d366
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-10-07 10:33:02 -0700

    Started load entry

--- app/controllers/AdminUpdateController.js
+++ app/controllers/AdminUpdateController.js
@@ -8,13 +8,14 @@ angular.module('tcg')
         $scope.selectedEntry = {id: -1, name: ""};
 
         $scope.formOut = "";
-        $scope.loadEntries = function() {
+        $scope.loadEntries = function () {
             $scope.formOut = "";
             $scope.selectedEntry = {id: -1, name: ""};
             GeneralAPI.list_min_portfolio().then(
                 function (success) {
                     $scope.portfolioEntries = success.data;
                     $scope.selectedEntry = $scope.portfolioEntries[0];
+                    $scope.loadEntry();
                 },
                 function (error) {
                     $scope.formOut = error.data.message;
@@ -22,10 +23,24 @@ angular.module('tcg')
             );
         };
 
-        $scope.loadEntry = function() {
+        $scope.technologies = [];
+
+        $scope.loadTechnologies = function () {
+            GeneralAPI.load_technologies().then(
+                function(success) {
+                    $scope.technologies = success.data;
+                },
+                function(error) {
+                    console.log(error);
+                }
+            );
+        };
+
+        $scope.loadEntry = function () {
             console.log($scope.selectedEntry);
         };
 
         $scope.loadEntries();
+        $scope.loadTechnologies();
 
     });
