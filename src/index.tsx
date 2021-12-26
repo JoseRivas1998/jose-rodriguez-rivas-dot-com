fdbaa8987b11defc5b87c52a3ba404c7df0df53c
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2021-12-26 11:25:30 -0800

    Bring in more libraries

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+import {BrowserRouter} from "react-router-dom";
+
 import App from './containers/App/App';
+
+import './styles/main.scss';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_ROOT;
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
