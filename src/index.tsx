ece361e786a58e6254c17e24cb30e92090e075c7
Jose Rodriguez Rivas <joserivas823@gmail.com>
2024-10-14 21:33:43 -0700

    Remove axios

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './containers/App/App';
 import './styles/main.scss';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-axios.defaults.baseURL = import.meta.env.VITE_API_ROOT;
 
 const queryClient = new QueryClient();
 
